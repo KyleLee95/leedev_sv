a379ca411131fc48c0d3a589e7462a190506cb80
Kyle Lee <kyle@kylelee.dev>
2024-12-20 12:30:57 -0500

    add basic root page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,68 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	const navItems = [
+		{ display: 'About', to: '/about' },
+		{ display: 'Blog', to: '/blog' }
+	];
+</script>
+
+<svelte:head>
+	<title>Kyle Lee</title>
+</svelte:head>
+
+<div class="container">
+	<div class="grid-item" id="sidebar">
+		<ul>
+			<li>
+				<h1>Kyle Lee</h1>
+			</li>
+			{#each navItems as navItem}
+				<li>
+					<a href={navItem.to}>{navItem.display}</a>
+				</li>
+			{/each}
+		</ul>
+	</div>
+
+	<main class="grid-item" id="content">main content here</main>
+</div>
+
+<style>
+	/* Remove bullet points from the list */
+	li {
+		list-style: none;
+	}
+
+	/* Style the sidebar */
+	#sidebar {
+		display: flex;
+		flex-direction: column;
+		background-color: #f4f4f4;
+		padding: 20px;
+	}
+
+	/* Style the main content */
+	#content {
+		background-color: #ffffff;
+		padding: 20px;
+		text-align: left;
+	}
+
+	/* Create the two-column layout */
+	.container {
+		display: grid;
+		grid-template-columns: 250px 1fr; /* Sidebar takes fixed width, content takes remaining space */
+		height: 100vh; /* Full viewport height */
+	}
+
+	.grid-item {
+		border: 1px solid #ddd;
+	}
+
+	/* Center the main content vertically */
+	#content {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		font-size: 20px;
+	}
+</style>
