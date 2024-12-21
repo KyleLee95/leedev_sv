d34f25131c712c674dbbc1284f05ae600257a0c9
kyle <kyle@kylelee.dev>
2024-12-20 23:25:41 -0500

    Fixed mobile menu hamburger menu rendering

--- src/lib/components/mobile-nav.svelte
+++ src/lib/components/mobile-nav.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import * as Sheet from './ui/sheet/index.js';
 	import { Menu } from 'lucide-svelte/icons';
-	import { Button } from './ui/button/index.js';
+	import { buttonVariants, Button } from './ui/button/index.js';
 	import MobileLink from './mobile-link.svelte';
 	import { ScrollArea } from './ui/scroll-area/index.js';
 	const navItems = [
@@ -18,12 +18,13 @@
 </script>
 
 <Sheet.Root bind:open>
-	<Sheet.Trigger>
-		<Button
-			variant="ghost"
-			class="mr-2 px-0 text-base hover:bg-transparent focus-visible:bg-transparent focus-visible:ring-0 focus-visible:ring-offset-0 md:hidden"
-		>
+	<Sheet.Trigger
+		class="mr-2 px-0 text-base hover:bg-transparent focus-visible:bg-transparent focus-visible:ring-0
+focus-visible:ring-offset-0 md:hidden"
+	>
+		<Button variant="ghost">
 			<Menu />
+
 			<span class="sr-only">Toggle Menu</span>
 		</Button>
 	</Sheet.Trigger>
