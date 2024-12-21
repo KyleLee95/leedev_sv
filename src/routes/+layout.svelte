cacedbe8983cbd5934b60a6fb24e17f75273f96c
kyle <kyle@kylelee.dev>
2024-12-21 00:50:43 -0500

    set default mode in mode watcher instead of weird header hack

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,18 +3,14 @@
 	import SiteHeader from '$lib/components/site-header.svelte';
 	import { ModeWatcher } from 'mode-watcher';
 	let { children } = $props();
-	import { browser } from '$app/environment';
 </script>
 
-{#if browser}
-	<ModeWatcher />
-{/if}
+<ModeWatcher defaultMode={'system'} />
 <div class="w-screen h-screen mx-auto">
 	<main class="w-full h-screen">
 		<SiteHeader />
 		<div class="container">
 			{@render children?.()}
-			<div></div>
 		</div>
 	</main>
 </div>
